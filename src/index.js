ee6f3734219a6c1d0704e35da06fd54e4e191515
Ayim William <odhisayim@gmail.com>
2025-04-16 12:37:50 +0300

    added link to each user profile

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Login from "./pages/Login";
+import UserProfile from "./pages/UserProfile";
 
 const router = createBrowserRouter([
     {
@@ -19,6 +20,10 @@ const router = createBrowserRouter([
         path: "/login",
         element: <Login />
     },
+    {
+        path: "/profile/:id",
+        element: <UserProfile />
+      }
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
